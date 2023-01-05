f580fb3954e527daeebf6114d6326959c5f689b0
The Anh Nguyen <ntheanh201@gmail.com>
2023-01-05 16:30:29 +0700

    add deployment argo simple app

---
+++ simple-argo/deploy.argo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git config --global user.email "ntheanh201@gmail.com"
+git config --global user.name "CircleCI + ArgoCD"
+
+echo "====== PULL ======"
+git pull
+
+echo "====== UPDATE IMAGE VERSION ======"
+echo $TAG
+sed -i "s/teamx-gitops-demo:.*/teamx-gitops-demo:$TAG/g" kustomize/overlays/dev/deployment_patch.yaml
+
+if [[ $(git status --porcelain) ]]; then
+  echo "====== COMMIT NEW CHANGES ======"
+  git add .
+  git commit -m "BACKEND: update image version $TAG"
+  git push origin master
+else
+  echo "====== NO CHANGES ======"
+fi
