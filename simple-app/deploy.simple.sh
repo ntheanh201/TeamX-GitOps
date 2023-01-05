05917e62ac7f2156ade6023a57f42c12e5001e35
The Anh Nguyen <ntheanh201@gmail.com>
2023-01-05 15:12:12 +0700

    add deployment patch

--- simple-app/deploy.simple.sh
+++ simple-app/deploy.simple.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-cho $TAG
+git config --global user.email "nthean201@gmail.com"
+git config --global user.name "Bot CI"
+
 echo "====== PULL ======"
 git pull
 
+echo "====== UPDATE IMAGE VERSION ======"
+echo $TAG
+sed -i "s/teamx-gitops-demo:.*/teamx-gitops-demo:$TAG/g" kustomize/deployment_patch.yaml
+
 echo "====== K8S ======"
-#cd kustomize
-#kustomize build . | k3s kubectl apply -f -
-kubectl rollout restart deployment/teamx-gitops-simple-app -n teamx
+cd kustomize
+kustomize build . | k3s kubectl apply -f -
+
+if [[ $(git status --porcelain) ]]; then
+  echo "====== COMMIT NEW CHANGES ======"
+  git add .
+  git commit -m "BACKEND: update image version $TAG"
+  git push origin master
+else
+  echo "====== NO CHANGES ======"
+fi
